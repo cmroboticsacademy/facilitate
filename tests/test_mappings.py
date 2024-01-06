6efe32700668015a499e028f4b38259910f60d18
ChrisTimperley <christimperley@googlemail.com>
2024-01-06 17:04:13 -0500

    test: added test_sources_and_destinations

--- tests/test_mappings.py
+++ tests/test_mappings.py
@@ -16,6 +16,27 @@ def tree_after(good_tree) -> Node:
     return good_tree
 
 
+def test_sources_and_destinations(tree_before: Node, tree_after: Node) -> None:
+    mappings = NodeMappings()
+
+    n1_before = tree_before.find(":input[DIRECTION]@0z(.tYRa{!SepmI$)#U,")
+    n1_after = tree_after.find(":input[DIRECTION]@tJ6ev+AvtHAf*PZbI}7i")
+
+    mappings.add(n1_before, n1_after)
+
+    sources = set(mappings.sources())
+    assert len(sources) == 1
+    assert n1_before in sources
+    assert mappings.source_is_mapped(n1_before)
+    assert mappings.source_is_mapped_to(n1_before) == n1_after
+
+    destinations = set(mappings.destinations())
+    assert len(destinations) == 1
+    assert n1_after in destinations
+    assert mappings.destination_is_mapped(n1_after)
+    assert mappings.destination_is_mapped_to(n1_after) == n1_before
+
+
 def test_add_nodes_with_different_types(tree_before: Node, tree_after: Node) -> None:
     node_before = tree_before.find(":input[RATE]@f7NN)mvnvpU?d$C(PZTk")
     node_after = tree_after.find(":field[SPIN_DIRECTIONS]@;UXN^kcB}()L,w3LFgoL")
