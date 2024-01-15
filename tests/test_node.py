3bf4a6d06d1726ad6eac4b6e678b9dc35365f6e7
ChrisTimperley <christimperley@googlemail.com>
2024-01-15 16:15:56 -0500

    test: added test_has_children to test_node

--- tests/test_node.py
+++ tests/test_node.py
@@ -9,3 +9,14 @@ def test_height(good_tree: Node) -> None:
 
     node = good_tree.find("0z(.tYRa{!SepmI$)#U,")
     assert node.height == 4
+
+
+def test_has_children(good_tree: Node) -> None:
+    node = good_tree.find(":input[DIRECTION]@0z(.tYRa{!SepmI$)#U,")
+    assert node.has_children()
+
+    node = good_tree.find("x:e}MT(JcdrCU9-b]!D?")
+    assert node.has_children()
+
+    node = good_tree.find(":field[SPIN_DIRECTIONS]@x:e}MT(JcdrCU9-b]!D?")
+    assert not node.has_children()
