e148c937e2fbdf0693b29d5f758601b70d44dd2a
ChrisTimperley <christimperley@googlemail.com>
2024-01-29 20:59:45 -0500

    tests: updated tests to not rely on prefix

--- tests/test_sequence.py
+++ tests/test_sequence.py
@@ -23,35 +23,35 @@ def test_insert_block() -> None:
 
     insert_block(id_="1", position=0)
     assert blocks_to_ids(sequence.blocks) == [
-        "ADDED:1",
+        "1",
     ]
 
     insert_block(id_="2", position=0)
     assert blocks_to_ids(sequence.blocks) == [
-        "ADDED:2",
-        "ADDED:1",
+        "2",
+        "1",
     ]
 
     insert_block(id_="3", position=1)
     assert blocks_to_ids(sequence.blocks) == [
-        "ADDED:2",
-        "ADDED:3",
-        "ADDED:1",
+        "2",
+        "3",
+        "1",
     ]
 
     insert_block(id_="4", position=2)
     assert blocks_to_ids(sequence.blocks) == [
-        "ADDED:2",
-        "ADDED:3",
-        "ADDED:4",
-        "ADDED:1",
+        "2",
+        "3",
+        "4",
+        "1",
     ]
 
     insert_block(id_="5", position=5)
     assert blocks_to_ids(sequence.blocks) == [
-        "ADDED:2",
-        "ADDED:3",
-        "ADDED:4",
-        "ADDED:1",
-        "ADDED:5",
+        "2",
+        "3",
+        "4",
+        "1",
+        "5",
     ]
