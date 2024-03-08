b5324ee31f7bb7eb15d0f144b9dd07cacf8bb85b
ChrisTimperley <christimperley@googlemail.com>
2024-03-08 17:07:17 -0500

    tests: added test for AssertionError@diff.py:273 fuzzer crash

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -21,6 +21,17 @@ def test_diff_literal_already_has_expression() -> None:
     script = compute_edit_script(tree_from, tree_to)
 
 
+@pytest.mark.xfail()
+def test_diff_update_is_not_none() -> None:
+    level_dir = _PATH_PROGRAMS / "spike_curric_sequential_movements_mini_challenge_curriculum"
+    student_dir = level_dir / "2952515"
+    before_file = student_dir / "12.json"
+    after_file = student_dir / "45.json"
+    tree_from = load_from_file(before_file)
+    tree_to = load_from_file(after_file)
+    script = compute_edit_script(tree_from, tree_to)
+
+
 def test_diff_minimal_trees(minimal_tree: Node, minimal_with_extra_tree: Node) -> None:
     tree_from = minimal_tree
     tree_to = minimal_with_extra_tree
